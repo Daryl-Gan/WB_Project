b32a760af40814c678fa8546717a7f88d56466b9
Daryl Gan <daryl.gan91@gmail.com>
2012-10-29 06:59:40 +0800

    Commit original project

---
+++ processor_v2_verilog/compile.do
@@ -0,0 +1,16 @@
+vlib work
+
+vlog -vlog01compat -work work multicycle.v
+vlog -vlog01compat -work work muxes.v
+vlog -vlog01compat -work work memory.v
+vlog -vlog01compat -work work DualMem.v
+vlog -vlog01compat -work work DataMemory.v
+vlog -vlog01compat -work work mux2to1.v
+vlog -vlog01compat -work work register_8bit.v
+vlog -vlog01compat -work work ALU.v
+vlog -vlog01compat -work work RF.v
+vlog -vlog01compat -work work extend.v
+vlog -vlog01compat -work work HEX.v
+vlog -vlog01compat -work work FSM.v
+vlog -vlog01compat -work work multicycle_tb.v
+
---
+++ processor_v2_verilog/memory.v
@@ -0,0 +1,63 @@
+// Copyright (C) 1991-2011 Altera Corporation
+// Your use of Altera Corporation's design tools, logic functions 
+// and other software and tools, and its AMPP partner logic 
+// functions, and any output files from any of the foregoing 
+// (including device programming or simulation files), and any 
+// associated documentation or information are expressly subject 
+// to the terms and conditions of the Altera Program License 
+// Subscription Agreement, Altera MegaCore Function License 
+// Agreement, or other applicable license agreement, including, 
+// without limitation, that your use is for the sole purpose of 
+// programming logic devices manufactured by Altera and sold by 
+// Altera or its authorized distributors.  Please refer to the 
+// applicable agreement for further details.
+
+// PROGRAM		"Quartus II 32-bit"
+// VERSION		"Version 11.1 Build 259 01/25/2012 Service Pack 2 SJ Web Edition"
+// CREATED		"Sun Oct 28 12:04:22 2012"
+
+module memory(
+	wren,
+	clock,
+	MemRead,
+	address,
+	data,
+	q
+);
+
+
+input wire	wren;
+input wire	clock;
+input wire	MemRead;
+input wire	[7:0] address;
+input wire	[7:0] data;
+output wire	[7:0] q;
+
+wire	SYNTHESIZED_WIRE_0;
+wire	[0:7] SYNTHESIZED_WIRE_1;
+wire	[7:0] SYNTHESIZED_WIRE_2;
+
+assign	SYNTHESIZED_WIRE_1 = 0;
+
+
+
+
+DataMemory	b2v_inst(
+	.wren(wren),
+	.clock(SYNTHESIZED_WIRE_0),
+	.address(address),
+	.data(data),
+	.q(SYNTHESIZED_WIRE_2));
+
+assign	SYNTHESIZED_WIRE_0 =  ~clock;
+
+
+mux2to1	b2v_inst3(
+	.sel(MemRead),
+	.data0x(SYNTHESIZED_WIRE_1),
+	.data1x(SYNTHESIZED_WIRE_2),
+	.result(q));
+
+
+
+endmodule
